fca12815aa2d3fa66619e8170157a27c86974346
Vadim <amputator.115@yandex.ru>
2023-03-24 12:31:30 +0300

    Числа с десятичной точкой

---
+++ float.py
@@ -0,0 +1,31 @@
+# Числа с десятичной точкой
+
+a = 45.78
+print(type(a))
+
+# Конвертация к целому числу
+
+temperatura = 36.6
+new_temp = int(temperatura)
+print(new_temp)
+print(type(new_temp))
+
+temperatura = int(36.6)
+print(temperatura)
+print(type(temperatura))
+
+# Конвертация строки к float
+
+temperatura = float('36.6')
+print(temperatura)
+print(type(temperatura))
+
+# Округление до целого числа round()
+
+temperatura = round(36.6)
+print(temperatura)
+print(type(temperatura))
+
+rate = 0.78
+print(round(rate))
+print(type(rate))
