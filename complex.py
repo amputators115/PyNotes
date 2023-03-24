306b384dcedfa70e8cb2ad0c3a05fe97a4fc204b
Vadim <amputator.115@yandex.ru>
2023-03-24 12:37:07 +0300

    Комлексные числа

---
+++ complex.py
@@ -0,0 +1,10 @@
+# Комлексные числа состоят из дейсвительной части и мнимой части
+
+complex_a = 35 + 10j
+complex_b = 24 + 7j
+print(complex_a + complex_b)
+print(complex_a - complex_b)
+print(complex_a * complex_b)
+print(complex_a / complex_b)
+print(type(complex_a + complex_b))
+
