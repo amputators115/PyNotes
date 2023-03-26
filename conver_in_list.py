72b5a60812aa79b4ab221d7098850bad4d97f06f
Vadim <amputator.115@yandex.ru>
2023-03-26 12:35:55 +0300

    Конвертация в список

---
+++ conver_in_list.py
@@ -0,0 +1,33 @@
+# Конвертация в список - втсроенная функция list()
+
+# Конвертация строки
+
+my_str = 'Hello Word!'
+print(my_str)
+print(type(my_str))
+print(len(my_str))
+print(id(my_str))
+
+new_str_list = list(my_str)
+print(new_str_list)
+print(type(new_str_list))
+print(len(new_str_list))
+print(id(new_str_list))
+
+# Конвертация cловаря в список
+
+my_dict = {'a': 10, 'b': 45}
+print(my_dict)
+print(type(my_dict))
+print(len(my_dict))
+print(id(my_dict))
+
+new_dict_in_list = list(my_dict)
+print(new_dict_in_list)
+print(type(new_dict_in_list))
+print(len(new_dict_in_list))
+print(id(new_dict_in_list))
+
+
+
+
