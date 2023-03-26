cce69bf05b8c79b4e20a2b297e4f7c7310a7a144
Vadim <amputator.115@yandex.ru>
2023-03-26 13:06:39 +0300

    Объединение и нарезка списков

---
+++ objed_narezka_list.py
@@ -0,0 +1,15 @@
+# Объединение списков
+
+my_num = [1, 56, 78, 678, 876.8]
+two_dict = ['Msc', 'ivan', 56, 78.9]
+print(my_num + two_dict)
+
+# Нарезка списков
+
+my_num = [1, 56, 78, 678, 876.8]
+print(my_num[:2])
+print(my_num[1:5])
+print(my_num[:2])
+print(my_num[-1])
+print(my_num[1:-1])
+print(my_num[-2])
