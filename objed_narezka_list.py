4631395a904287719d81b3acdbd12e58cf7fa944
Vadim <amputator.115@yandex.ru>
2023-03-26 13:47:50 +0300

    Объединение,нарезка, копирование списков

--- objed_narezka_list.py
+++ objed_narezka_list.py
@@ -13,3 +13,78 @@ print(my_num[:2])
 print(my_num[-1])
 print(my_num[1:-1])
 print(my_num[-2])
+
+# Копирование списков
+# При таком способе прлучаем полные копии
+my_num = [1, 56, 78, 678, 876.8]
+print(my_num)
+print(id(my_num))
+new_num = my_num
+print(id(new_num))
+
+# При добавлении элемента в новый список происходит мутация исходника
+
+my_num = [1, 56, 78, 678, 876.8]
+print(my_num)
+print(id(my_num))
+print(len(my_num))
+
+new_num = my_num
+new_num.append('vova')
+print(my_num)
+print(new_num)
+print(id(new_num))
+print(len(new_num))
+print(new_num == my_num)
+print(id(new_num) == id(my_num))
+
+# Копирование без мутации
+
+# 1 Метод [:] - копирование через нарезку
+
+my_num = [1, 56, 78, 678, 876.8]
+print(my_num)
+print(id(my_num))
+print(len(my_num))
+
+new_num = my_num[:] #копируем объект
+new_num.append('vova')
+print(my_num)
+print(new_num)
+print(id(new_num))
+print(len(new_num))
+print(new_num == my_num)
+print(id(new_num) == id(my_num))
+
+# 2 Копирование через метод copy() 
+
+my_num = [1, 56, 78, 678, 876.8]
+print(my_num)
+print(id(my_num))
+print(len(my_num))
+
+new_num = my_num.copy()  # копируем объект
+new_num.append('vova')
+print(my_num)
+print(new_num)
+print(id(new_num))
+print(len(new_num))
+print(new_num == my_num)
+print(id(new_num) == id(my_num))
+
+# 2 Копирование через встроенную функцию list()
+
+my_num = [1, 56, 78, 678, 876.8]
+print(my_num)
+print(id(my_num))
+print(len(my_num))
+
+new_num = list(my_num)  # копируем объект
+new_num.append('vova')
+print(my_num)
+print(new_num)
+print(id(new_num))
+print(len(new_num))
+print(new_num == my_num)
+print(id(new_num) == id(my_num))
+
