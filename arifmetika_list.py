691c733348151aea1b87268ea7c6dc1f7fe8642e
Vadim <amputator.115@yandex.ru>
2023-03-26 12:55:50 +0300

    Арифметические операции в списках

---
+++ arifmetika_list.py
@@ -0,0 +1,14 @@
+# Арифметические операции в списках
+
+ratings = [4.6, 7.8, 3, 8.9, 2.1]
+print(min(ratings))
+print(max(ratings))
+print(sum(ratings))
+print(sum(ratings)/len(ratings))
+print(ratings[0] + ratings[3])
+print(ratings[0] / ratings[3])
+print(ratings[0] * ratings[3])
+print(ratings[0] ** ratings[3])
+print(ratings[0] % ratings[3])
+print(ratings[4] // ratings[1])
+
