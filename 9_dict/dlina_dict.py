3f75f0b3ccac5a93185bed1547e0b8a1a545168e
Vadim <amputator.115@yandex.ru>
2023-03-28 13:54:09 +0300

    Определение длины словаря

---
+++ 9_dict/dlina_dict.py
@@ -0,0 +1,15 @@
+# Длина словаря len
+
+auto = {
+	'brand': 'Lexus',
+	'year': 2022,
+	'color': 'red',
+	'engine': 4.5,
+	'price_info': {
+		'price': 25000,
+		'is_avaible': True
+	}
+}
+
+print(len(auto))
+
