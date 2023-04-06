2821558259880dca6c4239f019ab03955081ce79
Vadim <amputator.115@yandex.ru>
2023-04-06 13:56:05 +0300

    Методы и Ковретация кортежей

--- 10_tuple/method_tuple.py
+++ 10_tuple/method_tuple.py
@@ -2,3 +2,27 @@
 
 my_tuple = (23, 567, 89)
 print(dir(my_tuple))
+
+# .count
+
+my_tuple = (23, 567, 89)
+print(my_tuple.count(567))
+
+# .index - индекс опрделенного элемента
+
+my_tuple = (23, 567, 89)
+print(my_tuple.index(23))
+
+# Конвертация кортежа в список
+
+my_tuple = (23, 567, 89)
+new_my_tuple = list(my_tuple)
+print(new_my_tuple)
+print(type(new_my_tuple))
+
+# Конвертация списка в кортеж
+
+my_list = [23, 567, 89]
+new_my_list = tuple(my_list)
+print(new_my_list)
+print(type(new_my_list))
