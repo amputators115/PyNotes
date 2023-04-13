f0524d9c6e079f6a55dfc6cdf39e575238b7baeb
Vadim <amputator.115@yandex.ru>
2023-04-13 15:07:02 +0300

    Функции

--- 15_Funcion.py
+++ 15_Funcion.py
@@ -4,6 +4,7 @@ def sum(a, b):
     c = a + b
     print(c)
 
+
 sum(2, 2)
 
 
@@ -11,14 +12,17 @@ def sum(a, b):
     c = a + b
     return c
 
+
 res = sum(2, 2)
 print(res)
 
 # Функция это объект
 
+
 def sum(a, b):
     c = a + b
     print(c)
+
 
 print(type(sum))
 print(dir(sum))
@@ -26,32 +30,89 @@ print(dir(sum))
 # Функция возвращает Nan - если нет return
 # Функция прекрает свою работу только после возвращение результата (return)
 # Параметры, return, аргументы - опционально можно не передавать
+
+
 def sum(a, b):
     c = a + b
-    
-sum(1,1)
+
+
+sum(1, 1)
 
 # Самая короткая функция (pass - когда вы не знаете что будет в теле функции)
 
+
 def sum():
-	pass
+    pass
+
+
 print(sum())
-    
+
 # Внутри функции не рекомендуется изменять внешние объекты
 # Лучше всего копировать .copy() внутри функции
 
+
 def increase_person_age(person):
-	person_copy = person.copy()
-	person_copy['age'] += 1
-	return person_copy
+    person_copy = person.copy()
+    person_copy['age'] += 1
+    return person_copy
+
 
 person_one = {
-	'name': 'Petr',
-	'age': 24
+    'name': 'Petr',
+    'age': 24
 }
 
 new_person = increase_person_age(person_one)
 print(new_person['age'])
 print(person_one['age'])
 
+# Может ли функция иметь любое количество аргументов?
+# Да может когда объедянеется в кортеж аругментов
+
+# Объединение элементов в tuple - кортеж оператор *
+
+
+def sum_nums(*args):
+    print(args)
+    print(type(args))
+    print(args[0])
+    return sum(args)
+
+
+print(sum_nums(2, 3, 7))
+
+# Позиционные аргументы (порядок следования важен)
+
+
+def post_info(name, post):
+    info = f'{name} wrote {post}'
+    return info
+
+
+info = post_info('Petr', 45)
+print(info)
+
+# Аргументы с ключевыми словами - код более читаем
+
+
+def post_info(name, post):
+    info = f'{name} wrote {post}'
+    return info
+
+
+info = post_info(name='Petr', post=45)
+print(info)
+
+# Объединение позиционных аргументов в словарь(dict) оператор **
+
+def post_info(**person):
+	
+    info = (
+		f"{person['name']} wrote " 
+		f"{person['post']} post"
+	)
+    return info
+
+info = post_info(name='Petr', post=45)
+print(info)
 
