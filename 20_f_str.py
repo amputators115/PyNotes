56a8ef655af277468665527852b3daf07609167f
Vadim <amputator.115@yandex.ru>
2023-04-15 17:41:21 +0300

    f- строки

---
+++ 20_f_str.py
@@ -0,0 +1,16 @@
+# f - строки
+# f'{} {}......{}'
+# Внутри фигурных скобок могут быть абсолютно любые выражение в PY
+
+a, b = 'Hello', 'Word!'
+greet = f'{a} {b}'
+print(greet)
+
+# Пример - Мое авто BMW cтоит 30 000
+
+brand = 'BMW'
+price = 30000
+
+auto_info = f'Мое авто {brand} cтоит {price}'
+print(auto_info)
+
